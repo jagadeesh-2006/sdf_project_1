1091b4cd8e9f4d6a283f61e8e3e15b8055d61400
Jagadeesh <jagadeeshmerugumala0747@gmail.com>
2025-05-01 22:21:38 +0530

    now done

--- run_Project.py
+++ run_Project.py
@@ -1,25 +1,4 @@
-# import subprocess
-# import sys
 
-# def build_and_run(args):
-#     # Compile Java files
-#     subprocess.run(["javac", "-d", "out", "arbitraryarithmetic/Afloat.java","arbitraryarithmetic/Ainteger.java", "MyInfArith.java"], check=True)
-
-#     # Create JAR file
-#     subprocess.run([
-#         "jar", "cf", "arithmetic.jar",
-#         "-C", "out", "arbitraryarithmetic",
-#         "-C", "out", "MyInfArith.class"
-#     ], check=True)
-
-#     # Run the main class
-#     subprocess.run(["java", "-cp", "arithmetic.jar", "MyInfArith"] + args)
-
-# if __name__ == "__main__":
-#     if len(sys.argv) < 5:
-#         print("Usage: python run_project.py <int/float> <add/sub/mul/div> <operand1> <operand2>")
-#     else:
-#         build_and_run(sys.argv[1:])
 import subprocess
 import sys
 import os
